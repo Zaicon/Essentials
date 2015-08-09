2ae4c6af6cbc731556f195083a39da41bf0faede
Zaicon Kiroshu <kzaicon@yahoo.com>
2015-08-09 14:21:50 -0500

    Supports API 1.21.

--- Essentials/esMain.cs
+++ Essentials/esMain.cs
@@ -13,7 +13,7 @@ using TShockAPI.Hooks;
 
 namespace Essentials
 {
-	[ApiVersion(1, 20)]
+	[ApiVersion(1, 21)]
 	public class Essentials : TerrariaPlugin
 	{
 		public override string Name { get { return "Essentials"; } }
